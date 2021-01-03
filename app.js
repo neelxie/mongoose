6f0e14addd06add05c9bc2258bb9ba8d3fc3af11
neelxie <kidricederek@gmail.com>
2021-01-03 17:17:13 +0300

    connect to db

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Book = require('./Book.model');
 
+var db = 'mongodb://localhost/example';
+
+mongoose.connect(db);
+
 var port = 8080;
 
 app.listen(port, function(){
     console.log('App listening on port ' + port);
-})
+});
