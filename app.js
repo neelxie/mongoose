74275e13072855efe3042708530b66295e86090c
neelxie <kidricederek@gmail.com>
2021-01-03 17:07:01 +0300

    import book model into app.js file

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Book = require('./Book.model');
 
 var port = 8080;
 
 app.listen(port, function(){
-    console.log('App listening on port 8080' + port);
+    console.log('App listening on port ' + port);
 })
